97eaaf14ac202db52695c61e67b61631345cffa7
siva@2204 <sivaraman26r@gmail.com>
2020-10-28 12:14:26 +0530

    [Fix]:Explore function logic

--- client/src/Components/Expolre.js
+++ client/src/Components/Expolre.js
@@ -1,5 +1,6 @@
 import React, {useState,useEffect} from 'react';
 import Commitment from './Commitment';
+import '../css/explore.css';
 
 function Explore({ins,accts}) {
   const[commitments,setCommitments] = useState([]);
@@ -8,10 +9,15 @@ function Explore({ins,accts}) {
     if (ins.methods) {
       const totalCommitments = await ins.methods.totalCommitments().call();
       const promise = [];
-      for (let i = 0; i < totalCommitments; i++) {
-        promise.push(ins.methods.commitments(i).call());
+      if (totalCommitments !== 0) {
+        for (let i = 0; i < totalCommitments; i++) {
+          promise.push(ins.methods.commitments(i).call());
+        }
+         Promise.all(promise).then((values)=>{setCommitments(values);}).catch((err)=>console.log(err));
+      }else{
+        setCommitments([]);
       }
-       Promise.all(promise).then((values)=>{setCommitments(values);}).catch((err)=>console.log(err));
+      
     }
   };
 
@@ -21,11 +27,10 @@ function Explore({ins,accts}) {
 
 return (
   <>
-    <h1>Commitments</h1>
+    {commitments == []?<h1 className = "heading">Oops no commitments!</h1>: <h1 className = "heading">Commitments</h1>}
     {commitments.map((commitment,i) => (
       <Commitment key = {i} contract = {commitment.Contract} contractor = {commitment.contractor} contractee = {commitment.contractee} amount = {commitment.amount}/>
     ))}
-
   </>
 );
 }
