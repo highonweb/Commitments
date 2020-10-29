3a2395d3d432a4f5a720fbe5be21c393a30ed53e
Mohan <102119043@nitt.edu>
2020-10-29 18:40:18 +0530

    Formatting and route pendingContract is completed

--- client/src/Components/PendingContracts.js
+++ client/src/Components/PendingContracts.js
@@ -1,9 +1,53 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
+import Commitment from './Commitment';
 
-function PendingContract() {
-  const greeting = "Pending Contracts!";
+function PendingContract({web3, ins, accts}) {
+  const [commitments, setCommitments] = useState([]);
 
-  return <h1>{greeting}</h1>;
+  const justAsync = async () => {
+    if (ins.methods) {
+      const totalCommitments = await ins.methods.totalCommitments().call();
+      const promise = [];
+      if (totalCommitments !== 0) {
+        for (let i = 0; i < totalCommitments; i++) {
+          promise.push(ins.methods.commitments(i).call());
+        }
+        Promise.all(promise)
+          .then((values) => {
+            setCommitments(values);
+          })
+          .catch((err) => console.log(err));
+      } else {
+        setCommitments([]);
+      }
+    }
+  };
+  useEffect(() => {
+    justAsync();
+  }, [ins, accts, web3]);
+
+  return (
+    <>
+      {commitments.map((commitment, i) => {
+        if (accts.includes(commitment.contractee)) {
+          return (
+            <Commitment
+              key={i}
+              assignedContracteeEvent={ins.events.AssignedContractee}
+              accts={accts[0]}
+              assignContractee={ins.methods.assigncontractee}
+              id={i}
+              web3={web3}
+              contract={commitment.Contract}
+              contractor={commitment.contractor}
+              contractee={commitment.contractee}
+              amount={commitment.amount}
+            />
+          );
+        }
+      })}
+    </>
+  );
 }
 
 export default PendingContract;
